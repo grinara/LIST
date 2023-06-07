a890a50a05749fb6f82691163788cc118ff23864
grinara <114216399+grinara@users.noreply.github.com>
2023-06-07 20:52:51 +0700

    Update Program.cs

--- LIST/Program.cs
+++ LIST/Program.cs
@@ -41,7 +41,10 @@ namespace Samogina_LAB3
                 S3.PRINT();
                 if (S3.is_empy()) { Console.WriteLine("S3-пуст"); }
                 Console.WriteLine("\n\n7)Создайте последовательность из 6 случайных чисел, меньших 20. Превратите ее в список S4 и выведите его на экран. Проверьте, есть ли в S4 элемент 25. Добавьте элемент 25 четвертым в список.");
-                Nodelist S4 = new Nodelist(6, 20);
+                int[] a = new int[6];
+                Random rnd = new Random();
+                for (int i=0; i<a.Length; i++) { a[i] = rnd.Next() % 20; }
+                Nodelist S4 = new Nodelist(a);
                 S4.PRINT();
                 if (S4.find(25) == null) { Console.WriteLine("нет 25"); }
                 S4.insert(3, 25);
@@ -54,7 +57,7 @@ namespace Samogina_LAB3
                 else { S5.earse_key(4); }
                 S5.PRINT();
                 Console.WriteLine("\n\n9)Измените S5, записав в него 4 числа: 11, 12, 13, 14 (числа вводятся с клавиатуры). Сравните S5 и S4.");
-                for(int i=0;i<4;i++) { S5.Input(); }
+                S5.Input();
                 S5.PRINT();
                 if (S5 == S4) { Console.WriteLine("S5==S4"); }
                 else Console.WriteLine("S5!=S4");
@@ -66,69 +69,56 @@ namespace Samogina_LAB3
             {
                 Console.WriteLine("\n\n\n1)Создайте множество S1, из 10 случайных чисел. Выведите S1 на экран (используя функцию Print).");
                 NodeSet S1= new NodeSet(10,20);
-                S1.Sort();
                 S1.PRINT();
                 Console.WriteLine("\n\n2)Создайте множество S2 и инициализируйте его (при создании) значением S1.  Выведите S2 на экран (используйте потоковый вывод). Проверьте равенство множеств S1  и  S2.");
                 NodeSet S2 = (NodeSet)S1.Clone();
-                S2.Sort();
                 S2.PRINT();
                 if (S2 == S1) { Console.WriteLine("S2==S1"); }
                 Console.WriteLine("\n\n3)Проверьте, есть ли в S1 элемент 5. Создайте множество S3, которое получается  удалением/добавлением из S1 элемента 5. Проверьте, что S1 и S3 – не равны.");
                 NodeSet S3;
                 if (S1.find(5) == null) { S3 = (NodeSet)S1.Clone(); S3 = S1.Add_new(5); }
                 else { S3=(NodeSet)S1.Clone();S3 = S1.Del_new(5); }
-                S3.Sort();
                 S3.PRINT();
                 if (S1!=S3) { Console.WriteLine("S1!=S3"); }
                 Console.WriteLine("\n\n4)Создайте пустое множество S4. Проверьте его на пустоту.  Добавьте в S4 последовательно числа 5, 10, 15, 5.  Выведите S4 на экран.");
                 NodeSet S4=new NodeSet();
                 if (S4.is_empy()) { Console.WriteLine("S4 - empy"); }
                 S4.Add(5); S4.Add(10); S4.Add(15); S4.Add(5);
-                S4.Sort();
                 S4.PRINT();
                 Console.WriteLine("\n\n5)Создайте пустое множество S5.  Инициализируйте его множеством S4.  Проверьте, что во множестве S5 есть элемент 15 и удалите его. Выведите получившееся множество на экран.");
                 NodeSet S5 = S4;
                 if (S5.find(15) != null) { S5.Del(15); }
-                S5.Sort();
                 S5.PRINT();
                 Console.WriteLine("\n\n6)Создайте список T, из 20 случайных чисел. Выведите T на экран. Создайте из T множество S6.  Выведите S6 на экран. Определите количество элементов в S6.");
                 Nodelist T=new Nodelist(20,50);
+                T.Sort();
                 T.PRINT();
                 NodeSet S6=new NodeSet();
                 S6.toNodeSet(T);
-                S6.Sort();
                 S6.PRINT();
-                Console.WriteLine(S6.GetSize());
+                Console.WriteLine(S6.Size);
                 Console.WriteLine("\n\n7)Найдите S7 – дополнение S6 до универсального. Найдите множество S8=S7∩S6.");//пересечение
                 NodeSet S7=S6.complement();
-                S7.Sort();
                 S7.PRINT();
                 NodeSet S8 = S7.intersection_new(S6);
-                S8.Sort();
                 S8.PRINT();
                 Console.WriteLine("\n\n8)Создайте множество S9={1,3,5,7,9,11,13,15,17,19,21,23,25,27,29}.  Найдите V1 =S7 ∩ S9,  V2 = S7 ∪ S9,  V3 = S7 \\ S9.\r\n");
                 NodeSet S9 = new NodeSet();
                 for(int i=-1; i<29;) { S9.Add(i+2);i += 2; }
-                S9.Sort();
                 S9.PRINT();
                 NodeSet V1=S7.intersection_new(S9);
                 NodeSet V2 = S7.unification_new(S9);
                 NodeSet V3 = S7.difference_new(S9);
-                V1.Sort();
-                V2.Sort();
-                V3.Sort();
                 V1.PRINT();
                 V2.PRINT();
                 V3.PRINT();
                 Console.WriteLine("\n\n9)Измените V1, объединив его с V3. Сравните V1  с S7.");
                 V1= (NodeSet)(V1.unification_new(V3)).Clone();
-                V1.Sort();
                 V1.PRINT();
-                if (V1 == S7) { Console.WriteLine("V1==S7"); }
+                if (V1==S7) { Console.WriteLine("V1==S7"); }
                 else { Console.WriteLine("V1!=S7"); }
                 Console.WriteLine("\n\n10)Измените множество V2, заменив его разностью V2 и V3. Сравните V2  с  S9");
                 V2= (NodeSet)(V2.difference_new(V3)).Clone();
-                V2.Sort();
                 V2.PRINT();
                 if (V1 == S7) { Console.WriteLine("V2==S9"); }
                 else { Console.WriteLine("V2!=S9"); }
@@ -141,10 +131,11 @@ namespace Samogina_LAB3
         protected int Data;
         protected NODE? Next;
         protected NODE? Prev;
-        public NODE(int data, NODE? next, NODE? prev) { Data = data; Next = next; Prev = prev; }
+        public NODE(int data, NODE? next=null, NODE? prev = null) { Data = data; Next = next; Prev = prev; }
         public NODE() { Data = default(int); Next = null; Prev = null; }
-        public NODE(int data) { Data = data; Prev = null; Next = null; }
-        public void Print() { Console.WriteLine(this.Data); }
+        public void Print() { 
+            Console.Write("{0} {1}",this.Data,"");
+        }
         public int data
         {
             get { return Data; }
@@ -173,6 +164,15 @@ namespace Samogina_LAB3
             {
                 Push_back(rnd.Next()%rang);
             }
+            Sort();
+        }
+        public Nodelist(int[] a)
+        {
+            for(int i = 0; i < a.Length; i++)
+            {
+                Push_back(a[i]);
+            }
+            Sort();
         }
         public Nodelist(Nodelist A)
         {
@@ -194,14 +194,14 @@ namespace Samogina_LAB3
             }
             return ptr;
         }
-        public int GetSize()
+        public int Size
         {
-            return n;
+            get { return n; }
         }
         public NODE Push_front(int data) // добавление в начало
         {
             NODE ptr = new NODE(data);
-            ptr.next=Head;
+            ptr.next = Head;
             if (Head != null) { Head.prev=ptr; }
             if (Tail == null) { Tail = ptr; }
             Head = ptr;
@@ -219,14 +219,14 @@ namespace Samogina_LAB3
             return ptr;
         }
         public void Pop_front()
-        { //удаление с головы
+        { 
             if (Head == null) return;
             NODE? ptr = Head.next;
             if (ptr != null) { ptr.prev=null; }
             else Tail = ptr;
             n--;
             Head = ptr;
-        }
+        }//удаление с головы
         public void Pop_back() //удаление с конца
         {
             if (Tail == null) return;
@@ -239,12 +239,12 @@ namespace Samogina_LAB3
         public NODE getAt(int index) //поиск по позиции
         {
             NODE? ptr = Head;
-            int n = 0;
-            while (n != index)
+            int k = 0;
+            while (k != index)
             {
                 if (ptr == null) return ptr;
                 ptr = ptr.next;
-                n++;
+                k++;
             }
             return ptr;
         }
@@ -338,7 +338,18 @@ namespace Samogina_LAB3
             return data;
         }//минимум
         public bool is_empy() { if (Head == null) return true; return false; } //проверка на пустоту
-        public void clear() { while (Head != null) { Pop_back(); } }//очистка
+        public void clear() { Head = null;Tail = null;n = 0; }//очистка
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || !this.GetType().Equals(obj.GetType())) return false;
+            if (n != ((Nodelist)obj).n) return false;
+            if (Head == null || ((Nodelist)obj).Head == null) return false;
+            for (int i = 0; i < n; i++)
+            {
+                if (find(((Nodelist)obj)[i].data) == null) return false;
+            }
+            return true;
+        }
         public static Nodelist operator +(Nodelist l1, Nodelist l2)
         {
             Nodelist list = new Nodelist();
@@ -350,28 +361,30 @@ namespace Samogina_LAB3
         } //перегрузка +
         public static bool operator == (Nodelist l1, Nodelist l2)
         {
-            if (l1.n != l2.n) return false;
-            if (l1.Head ==null || l2.Head == null) return false;
-            for (int i = 0; i < l1.n; i++)
-            {
-                if (l1.find(l2[i].data) == null) return false;
-            }
-            return true;
+            return l1.Equals(l2);
         } //перегрузка ==
         public static bool operator !=(Nodelist l1, Nodelist l2)
         {
-            return !(l1 == l2);
+            return !(l1.Equals(l2));
         } //перегрузка !=
         public void PRINT()
         {
             NODE? ptr = Head;
-            while (ptr != null) { Console.Write(ptr.data); Console.Write(" "); ptr = ptr.next; }
+            while (ptr != null) { ptr.Print(); ptr = ptr.next; }
             Console.WriteLine();
         }
         public void Input()
         {
-            var a = int.Parse(Console.ReadLine());
-            Push_back(a);
+            var str = Console.ReadLine();
+            if (str != null)
+            {
+                string[] words = str.Split(' ');
+                foreach (string x in words)
+                {
+                    Push_back(int.Parse(x));
+                }
+            }
+            Sort();
         }
         public NODE this[int index]
         {
@@ -382,33 +395,18 @@ namespace Samogina_LAB3
         } //перегрузка []
         public void Sort()
         {
-            if (this.Head != null)
+            Nodelist? ptr = (Nodelist)this.Clone();
+            this.clear();
+            while (!ptr.is_empy())
             {
-                int j = 0;
-                int tmp = 0;
-                for (int i = 0; i < n; i++)
-                {
-                    j = i;
-                    for (int k = i; k < n; k++)
-                    {
-                        if (this[j].data > this[k].data)
-                        {
-                            j = k;
-                        }
-                    }
-                    tmp = this[i].data;
-                    this[i].data=this[j].data;
-                    this[j].data=tmp;
-                }
+                var a = ptr.Min().data;
+                Push_back(a);
+                ptr.earse_key(a);
             }
-        }  //сортировка
+        }//сортировка
     }
     class NodeSet : Nodelist {
-       // protected Nodelist Nodelist;
-        public int GetSize()
-        {
-            return n;
-        }
+
         public NodeSet() { n = 0; }
         public NodeSet(int size,int rang)
         {
@@ -419,11 +417,11 @@ namespace Samogina_LAB3
                 if (find(w) == null) { Add(w);i++; }
                 
             }
-            Sort();
+           // Sort();
         }
         public void toNodeSet(Nodelist ptr)
         {
-            for (int i = 0; i < ptr.GetSize(); i++)
+            for (int i = 0; i < ptr.Size; i++)
             {
                 Add(ptr[i].data);
             }
@@ -431,9 +429,16 @@ namespace Samogina_LAB3
         }
         new public void Input()
         {
-            var a = int.Parse(Console.ReadLine());
-            Add(a);
-          //  Sort();
+            var str = Console.ReadLine();
+            if (str != null)
+            {
+                string[] words = str.Split(' ');
+                foreach (string x in words)
+                {
+                    Add(int.Parse(x));
+                }
+            }
+            //  Sort();
         }
         new public object Clone()
         {
